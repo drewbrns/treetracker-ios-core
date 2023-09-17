4d64ee22d929e9e5cc6da5a8fd2c46c2afccc575
Alex Cornforth <alexcornforth@me.com>
2023-09-17 11:54:58 +0100

    Added POST message request

---
+++ treetracker-core/Models/Messaging/NewMessage.swift
@@ -0,0 +1,45 @@
+//
+//  NewMessage.swift
+//  Treetracker-Core
+//
+//  Created by Alex Cornforth on 17/09/2023.
+//
+
+import Foundation
+
+struct NewMessage {
+    
+    let messageId: String
+    let parentMessageId: String
+    let authorHandle: String
+    let recipientHandle: String?
+    let subject: String
+    let body: String
+    let composedAt: Date
+    let surveyResponse: [String]?
+    let surveyId: String?
+    let videoLink: String?
+
+    init(
+        parentMessageId: String,
+        authorHandle: String,
+        recipientHandle: String? = nil,
+        subject: String,
+        body: String,
+        composedAt: Date,
+        surveyResponse: [String]? = nil,
+        surveyId: String? = nil,
+        videoLink: String? = nil
+    ) {
+        self.messageId = UUID().uuidString
+        self.parentMessageId = parentMessageId
+        self.authorHandle = authorHandle
+        self.recipientHandle = recipientHandle
+        self.subject = subject
+        self.body = body
+        self.composedAt = composedAt
+        self.surveyResponse = surveyResponse
+        self.surveyId = surveyId
+        self.videoLink = videoLink
+    }
+}
